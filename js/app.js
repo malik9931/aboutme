c4f9d8389341ecf09e67a6d3d055394156db62c5
Malek <engmalek.civil@gmail.com>
2021-01-05 17:53:16 +0200

    Lab 03 code 201

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-/*
-'use strict';
 
+'use strict';
+var sccore = 0;
 var userName = prompt ('Welcome to my website, Can U tell me ur name,pls?');
 var userName = getValidation(userName);
 alert('Nice to meet U '+userName);
@@ -32,8 +32,61 @@ var cat = prompt('Do u think that I like cat?');
 var cat = getValidation(cat);
 var cat = result(cat);
 
+//the 6th qustion
 
-alert('Thank U '+userName+', You have made good attempts to answer questions');
+var age = 27;
+var guess = prompt("Guess my age, U have 4 attempts ");
+guess= getValidation(guess);
+guess = parseInt(guess);
+console.log(guess);
+var i = 1;
+
+do {
+    if (parseInt(guess) > age) {
+        guess = prompt("You've guessed too high!");
+        i++;
+    } else if (parseInt(guess) < age) {
+        guess = prompt("You've guessed too low!");
+        i++;
+    } else if(parseInt(guess) === age){
+         alert("Good Job!");
+        sccore++ ; }
+    
+
+} while (parseInt(guess) !== age && i<=3);
+if (parseInt(guess) !== age && i>3) {
+    alert('Sorr, u did not giss my age is: 27 ');
+}
+
+
+// the 7th qustion
+
+var num = [1,2,3];
+var gNum = prompt("Guess one of my 3 fav numbers from 1 to 10, u have 6 attempts");
+gNum= getValidation(gNum);
+gNum = parseInt(gNum);
+console.log(gNum);
+var z = 0;
+
+for (z=0; z<6; z++) {
+    switch (gNum) {
+        case 1:
+        case 2:
+        case 3:
+            alert('Correct Answer');
+            z =6;
+            sccore++;
+            alert('My first 3 fav numbers from 1 to 10 are: '+ num);
+            break;
+        default:
+            gNum = prompt("u have to try again: ");
+            break;
+        
+    } 
+    }
+
+
+alert('Thank U '+userName+', You have made good attempts to answer questions, ur sccore was: '+ sccore+' out of 7');
 
 
 
@@ -54,7 +107,8 @@ function result(userResult){
 
     if(userResult === 'yes' || userResult.toLowerCase() === 'yes' || userResult.toLowerCase() === 'y'){
 
-        alert('Very good, I like a Green color');
+        alert('Very good, This a correct answer');
+        sccore ++;
 
     }else{
         alert('Oops thats wrong');
@@ -64,4 +118,3 @@ function result(userResult){
 
 }
 
-*/
