ec975f96c25a96d144486e36986327515e6078b0
Malek <engmalek.civil@gmail.com>
2021-01-06 17:27:10 +0200

    doing some changes

--- js/app.js
+++ js/app.js
@@ -1,120 +1,170 @@
 
 'use strict';
-var sccore = 0;
+
 var userName = prompt ('Welcome to my website, Can U tell me ur name,pls?');
-var userName = getValidation(userName);
+
 alert('Nice to meet U '+userName);
 
+var score = 0;
 
 var color = prompt('Do u think that I like a Green color?');
 var color = getValidation(color);
-var color = result(color);
+var color = result(color,'great this is my color','NOOOOOOOOOOO');
 
 //---------------------------
 var drink = prompt('Do u think that I like Tea?');
 var drink = getValidation(drink);
-var drink = result(drink);
+var drink = result(drink,'I LOVE IT','NOOOOOOOOOOO');
 
 
 //-------------------------
 
 var swim = prompt('Do u think that I can swim?');
 var swim = getValidation(swim);
-var swim = result(swim);
+var swim = result(swim,'water is life','NOOOOOOOOOOO');
 
 //----------------------------
 var python = prompt('Do u think that I can programming by python?');
 var python = getValidation(python);
-var python = result(python);
+var python = result(python,'its amazing','NOOOOOOOOOOO');
 
 //----------------------
 var cat = prompt('Do u think that I like cat?');
 var cat = getValidation(cat);
-var cat = result(cat);
+var cat = result(cat,'Its a qute animal','NOOOOOOOOOOO');
 
 //the 6th qustion
 
+
 var age = 27;
 var guess = prompt("Guess my age, U have 4 attempts ");
-guess= getValidation(guess);
 guess = parseInt(guess);
 console.log(guess);
-var i = 1;
-
-do {
-    if (parseInt(guess) > age) {
-        guess = prompt("You've guessed too high!");
-        i++;
-    } else if (parseInt(guess) < age) {
-        guess = prompt("You've guessed too low!");
-        i++;
-    } else if(parseInt(guess) === age){
-         alert("Good Job!");
-        sccore++ ; }
-    
-
-} while (parseInt(guess) !== age && i<=3);
-if (parseInt(guess) !== age && i>3) {
-    alert('Sorr, u did not giss my age is: 27 ');
-}
+var ageGiss = numberGiss(guess,age);
 
 
 // the 7th qustion
 
-var num = [1,2,3];
+var num = ['1','2','3'];
 var gNum = prompt("Guess one of my 3 fav numbers from 1 to 10, u have 6 attempts");
-gNum= getValidation(gNum);
-gNum = parseInt(gNum);
-console.log(gNum);
-var z = 0;
-
-for (z=0; z<6; z++) {
-    switch (gNum) {
-        case 1:
-        case 2:
-        case 3:
-            alert('Correct Answer');
-            z =6;
-            sccore++;
-            alert('My first 3 fav numbers from 1 to 10 are: '+ num);
-            break;
-        default:
-            gNum = prompt("u have to try again: ");
-            break;
-        
-    } 
-    }
+var reqResult = arrayFunction(num,gNum);
 
 
-alert('Thank U '+userName+', You have made good attempts to answer questions, ur sccore was: '+ sccore+' out of 7');
-
+alert('Thank U '+userName+', You have made good attempts to answer questions, ur sccore was: '+ score+' out of 7');
 
 
 
 // validation Function
 function getValidation(userInput){
 
-    while(userInput === undefined || userInput === '' || userInput === null){
-
-        userInput = prompt('Can U write the answer again, pls?');
+    while (userInput === null || userInput === undefined || userInput === '') {
+      alert('please answer');
+        userInput = prompt('U have to put an answer?');
+    // console.log(license)
     }
-    return userInput;
-
-}
-
-// Result Function
-function result(userResult){
-
-    if(userResult === 'yes' || userResult.toLowerCase() === 'yes' || userResult.toLowerCase() === 'y'){
-
-        alert('Very good, This a correct answer');
-        sccore ++;
-
-    }else{
-        alert('Oops thats wrong');
-    }
-
     
+    while (userInput.toLowerCase() !== 'yes' && userInput.toLowerCase() !== 'no' && userInput.toLowerCase() !== 'y' && userInput.toLowerCase() !== 'n')
+    {
+        alert('please answer');
+          userInput = prompt('U have to put yes / no / y / n?');
+      // console.log(license)
+      }
+      return userInput;
+    
+  }
+  
+  // Result Function
+  function result(userResult,massege1,massege2){
+  
+   
+    if (userResult.toLowerCase() === 'yes' || userResult.toLowerCase() === "y" )  {
+      console.log(userResult.toLowerCase() === 'yes' || userResult.toLowerCase() === 'yes');
+      alert(massege1);
+      score++;
+    }
+  
+    else if (userResult.toLowerCase() === "no" || userResult.toLowerCase() === "n"){
+      alert(massege2);
+    // console.log(license)
+    }
+    
+  
+  }
+  
+  
 
-}
-
+  // Gissing Number Function
+  
+  
+  
+  function numberGiss(userNumber,myNumber) {
+    
+  var i;
+  for (i = 1; i <= 4; i++) {
+    console.log(userNumber);
+    if (userNumber == myNumber) {
+      alert('Right its' + myNumber)
+      console.log('true');
+      score++;
+      break;
+    }
+    else if (userNumber > myNumber) {
+      alert('sorry that is too hight you try ' + i + ' time')
+      
+      console.log('false');
+    }
+    else if (userNumber < myNumber) {
+      alert('sorry that is too low you try ' + i + ' time')
+      console.log('false');
+    }
+   userNumber = prompt('try another time a to guess my age, you have' + i + 'try')
+   
+  if(userNumber == myNumber){
+    alert('correct')
+    
+    break;
+  }
+   if(i==3){
+    alert('sorry, The answer is ' + myNumber);
+    break; 
+  }
+  }
+    
+  }
+  
+  
+  // Array Function
+  
+  function arrayFunction(myArrayCar,usercar) {
+    var z = 0;
+    var correctAnswer=false;
+    
+    while(z<=5 && !correctAnswer){
+      
+      for(var a = 0; a < myArrayCar.length; a++){
+        if(usercar===myArrayCar[a]){
+      
+          alert('that is correct');
+  
+          correctAnswer=true;
+          score++;
+          
+  
+  
+          break;
+  
+        }
+  
+    
+  
+      }
+  
+    
+    z++;
+  }
+  
+  alert(' the answers is '+ myArrayCar );
+  
+  
+    
+  }
